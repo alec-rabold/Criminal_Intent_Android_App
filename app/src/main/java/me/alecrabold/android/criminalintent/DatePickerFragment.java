536803eb2b48424d04ada1a422a09ae06864e350
Alec Rabold <alecrabold@outlook.com>
2017-05-31 13:11:56 -0700

    Initial commit

---
+++ app/src/main/java/me/alecrabold/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package me.alecrabold.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by Alec on 5/28/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
---
+++ app/src/main/java/me/alecrabold/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package me.alecrabold.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Alec on 5/18/2017.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment(); // This was wrong in the book (?) pg. 175
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
